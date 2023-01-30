1a09e0ad25832ac43ee0229d29220befa255e6a1
imChipy <chipymiguel@yahoo.com>
2023-01-29 23:03:35 -0600

    pushing latest

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,45 @@
 import Container from "react-bootstrap/Container";
 
 function About() {
-  return <Container className="about"></Container>;
+  return (
+    <Container className="about">
+      <div className="aboutLeft">
+        <img
+          alt=""
+          src="src\assets\aboutUs\aboutUs-img.png"
+          className="aboutLeftImg"
+        />
+      </div>
+      <div className="aboutRight">
+        <h1 className="aboutRightTitle">About Draft Labs</h1>
+        <h2 className="aboutRightSubtitle">A GameFi Company</h2>
+        <p className="aboutRightText">
+          Draft Labs is committed to integrating all aspects of Web3 into sports
+          gaming. We help users monetize their time and skill unlike previously
+          possible through our discrete blockchain integration and
+          user-friendliness, developing a game template that anyone can enjoy
+          while they "contribute-to-earn."
+        </p>
+        <div className="aboutRightSocials">
+          <img
+            alt=""
+            src="src\assets\aboutUs\Vector-1.png"
+            className="socialIcon"
+          />
+          <img
+            alt=""
+            src="src\assets\aboutUs\Vector-2.png"
+            className="socialIcon"
+          />
+          <img
+            alt=""
+            src="src\assets\aboutUs\Vector.png"
+            className="socialIcon"
+          />
+        </div>
+      </div>
+    </Container>
+  );
 }
 
 export default About;
