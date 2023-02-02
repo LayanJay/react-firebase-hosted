a582d688e2278135ef8f4a18e49d39f04296b2b5
imChipy <chipymiguel@yahoo.com>
2023-02-02 11:08:12 -0600

    last commit was from some idiot, this one will work

--- src/components/GameplayDemo.tsx
+++ src/components/GameplayDemo.tsx
@@ -1,21 +1,15 @@
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import "./styles/GameplayDemo.scss";
+import rulerLeft from "../assets/landing_section/ruler_pattern_left.png";
+import rulerRight from "../assets/landing_section/ruler_pattern_right.png";
 
 function GameplayDemo() {
   return (
     <Container className="gameplayDemoContainer" id="gameplay">
       <h1 className="gameplayDemoTitle">GAMEPLAY DEMO</h1>
-      <img
-        alt=""
-        src="/src/assets/landing_section/ruler_pattern_right.png"
-        className="gameplayRulerLeft"
-      />
-      <img
-        alt=""
-        src="/src/assets/landing_section/ruler_pattern_right.png"
-        className="gameplayRulerRight"
-      />
+      <img alt="" src={rulerLeft} className="gameplayRulerLeft" />
+      <img alt="" src={rulerRight} className="gameplayRulerRight" />
     </Container>
   );
 }
