cdab67e211566ef8175f1e4971acafbd5ac81d47
ParagonTheDev <nipuna.sudha@gmail.com>
2023-02-10 02:45:12 +0530

    feat: Add gameplay section with links

--- src/components/GameplayDemo.tsx
+++ src/components/GameplayDemo.tsx
@@ -2,13 +2,22 @@ import Container from "react-bootstrap/Container";
 import "./styles/GameplayDemo.scss";
 import rulerLeft from "../assets/landing_section/ruler_pattern_left.png";
 import rulerRight from "../assets/landing_section/ruler_pattern_right.png";
+import gameplayBg from "../assets/gameplay_demo/gameplay-bg.png";
+import playNowButton from "../assets/gameplay_demo/play-now-button.png";
 
 function GameplayDemo() {
     return (
         <Container className="gameplayDemoContainer" id="gameplay">
-            <h1 className="gameplayDemoTitle">GAMEPLAY DEMO</h1>
             <img alt="" src={rulerLeft} className="gameplayRulerLeft"/>
             <img alt="" src={rulerRight} className="gameplayRulerRight"/>
+                    <a href="https://demo.draftables.io/" target={'_blank'}>
+            <Container>
+                <div className={'gameplayView'}>
+                    <img alt="" src={gameplayBg} className="gameplayViewBg"/>
+                        <img alt="" src={playNowButton} className="gameplayButton"/>
+                </div>
+            </Container>
+                    </a>
         </Container>
     );
 }
