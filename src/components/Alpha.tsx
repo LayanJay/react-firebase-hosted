7691daca8ba3d6c6957a61cf3b000403dbde6b78
imChipy <chipymiguel@yahoo.com>
2023-02-20 13:26:25 -0600

    alpha section, added working contact form

--- src/components/Alpha.tsx
+++ src/components/Alpha.tsx
@@ -1,6 +1,7 @@
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import "./styles/Alpha.scss";
+import EmailForm from "./EmailForm";
 
 function Alpha() {
   return (
@@ -11,15 +12,7 @@ function Alpha() {
           Sign up for access to Alpha Release
         </h2>
       </Container>
-      <div className="alphaForm">
-        <input
-          className="alphaFormField"
-          type="text"
-          name="email"
-          placeholder="Example@gmail.com"
-        />
-        <Button className="buttonGeneral buttonGeneralSignup">SIGN UP</Button>
-      </div>
+      <EmailForm />
     </Container>
   );
 }
