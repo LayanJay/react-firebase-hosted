1a169b6f48c08642481df97fc59e63fbeab0609f
Dulmina Wijegunarathne <92733749+Dulmina98@users.noreply.github.com>
2023-07-22 19:36:12 +0530

    chore: update styles of own you team section

--- src/components/OwnYourTeam.tsx
+++ src/components/OwnYourTeam.tsx
@@ -1,31 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
 import "./styles/OwnYourTeam.scss";
 import dotPattern1 from "../assets/dot_pattern_1.png";
 import oytFootball1 from "../assets/ownyourteam/american_football.png";
 import oytFootball2 from "../assets/ownyourteam/american_football_2.png";
 import oytFootball3 from "../assets/ownyourteam/american_football_3.png";
-import oytVs from "../assets/ownyourteam/vs.png";
+import oytFootball4 from "../assets/ownyourteam/lightpaper.png";
 import litepaper from "../assets/litepaper.pdf";
+import dotPattern5 from "../assets/dot_pattern_5.png";
+import dotPattern6 from "../assets/dot_pattern_6.png";
 
 let lightpaperPDFLink = JSON.stringify(litepaper);
 let lightpaperPDFLinkFinal =
-  lightpaperPDFLink + "#toolbar=0&navpanes=0&scrollbar=0";
+    lightpaperPDFLink + "#toolbar=0&navpanes=0&scrollbar=0";
 
 function OwnYourTeam() {
   return (
-    <Container className="oytContainer" id="whatis">
-      <h2 className="sectionTitle">OWN. YOUR. TEAM.</h2>
-      <p className="oytDescription">
-        Draftables is the most customizable, rewarding, and in-depth sports
-        simulator of all time. You no longer have to pretend to be a football
-        franchise owner — you are one.
-      </p>
-      <div className="oytInnerContainer">
-        <img alt="" src={dotPattern1} className="dotPatternOyt" />
-        <img alt="" src={dotPattern1} className="dotPatternOyt2" />
-        {/* <div className="oytAssets">
+      <Container className="oytContainer" id="whatis">
+        <h2 className="sectionTitle">OWN. YOUR. TEAM.</h2>
+        <p className="oytDescription">
+          Draftables is the most customizable, rewarding, and in-depth sports
+          simulator of all time. You no longer have to pretend to be a football
+          franchise owner — you are one.
+        </p>
+        <div className="oytInnerContainer">
+          <img alt="" src={dotPattern1} className="dotPatternOyt"/>
+          <img alt="" src={dotPattern1} className="dotPatternOyt2"/>
+          <img src={dotPattern5} alt="" className={"dotPatterRight"}/>
+          <img src={dotPattern6} alt="" className={"dotPatterRightBottom"}/>
+          {/* <div className="oytAssets">
           <div className="oytImage">
             <img src={oytFootball1} className="whatIsImage" alt="" />
             <p className="imageParagraph">
@@ -110,7 +113,7 @@ function OwnYourTeam() {
           <div>
             <div className="oytImage">
               <div className="">
-                <img alt="" src={oytVs} className="whatIsImage" />
+                <img alt="" src={oytFootball2} className="whatIsImage"/>
               </div>
               <p className="imageParagraph">COMPETE AGAINST OTHER USERS
               </p>
@@ -124,9 +127,8 @@ function OwnYourTeam() {
 
           <div>
             <div className="oytImage">
-              <img src={oytFootball3} className="whatIsImage" alt="" />
-              <p className="imageParagraph">EARN $DRAFT TO BENEFIT YOU AND YOUR TEAM
-              </p>
+              <img src={oytFootball3} className="whatIsImage" alt=""/>
+              <p className="imageParagraph">BUILD YOUR OWN STADIUM VILLAGE</p>
               <p className="imageParagraphHover">
                 $DRFT is more than just an in-game currency: Earn it by
                 competing against other users, spend it to purchase new players
@@ -138,7 +140,7 @@ function OwnYourTeam() {
 
           <div>
             <div className="oytImage">
-              <img src={oytFootball2} className="whatIsImage" alt="" />
+              <img src={oytFootball4} className="whatIsImage" alt=""/>
               <p className="imageParagraph">READ OUR LITEPAPER</p>
               <p className="imageParagraphHover">
                 Draftables expands what it means for a game to be a sports
@@ -146,32 +148,6 @@ function OwnYourTeam() {
                 game in our litepaper.
               </p>
             </div>
-          </div>
-        </div>
-
-        <div className="oytLitePaper" id="litepaper">
-          <p className="oytDescription">
-            Draftables aims to fully realize the potential of sports simulators.
-            Experience unlimited control, true ownership, season-to-season
-            continuity, and a lively community. Draftables is a fun,
-            free-to-play game that solves the issues with current sports video
-            games.
-          </p>
-          {/* 
-          <div className="litepaperEmbed">
-            <object
-              data={litepaper}
-              type="application/pdf"
-              width="100%"
-              height="100%"
-              className="litepaperLink"
-            ></object>
-          </div>
-  */}
-          <div className="litepaperButton">
-            <a href={litepaper} target="_blank">
-              <Button className="buttonGeneral">LITEPAPER</Button>
-            </a>
           </div>
         </div>
       </div>
