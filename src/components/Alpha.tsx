6ac3d1a25cc942fe12c7d12daecd306cfbff45e8
Dulmina Wijegunarathne <92733749+Dulmina98@users.noreply.github.com>
2023-07-22 13:50:15 +0530

    chore: update styles of alpha section

--- src/components/Alpha.tsx
+++ src/components/Alpha.tsx
@@ -1,19 +1,17 @@
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import "./styles/Alpha.scss";
-import EmailForm from "./EmailForm";
+import React from "react";
 
 function Alpha() {
-  return (
-    <Container className="alphaContainer" id="alpha">
-      <Container className="alphaAssets">
-        <h3 className="sectionTitle">Alpha Release</h3>
-        <p>Sign up for access to Alpha Release
-        </p>
-      </Container>
-      <EmailForm />
-    </Container>
-  );
+    return (
+        <Container className="alphaContainer" id="alpha">
+            <Container className="alphaAssets">
+                <div className={"alphaText"}><span className={"alphaClickText"}>Click</span> to apply for the</div>
+                <Button className="buttonGeneral buttonAlpha">Closed Alpha Release</Button>
+            </Container>
+        </Container>
+    );
 }
 
 export default Alpha;
