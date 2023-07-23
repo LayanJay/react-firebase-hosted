6f375004a93b2b27e16397cdc112f21552caca17
Dulmina Wijegunarathne <92733749+Dulmina98@users.noreply.github.com>
2023-07-23 09:58:37 +0530

    chore: update styles of partners section

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,30 +1,35 @@
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import "./styles/Partners.scss";
-import dotPattern1 from "../assets/dot_pattern_1.png";
-import dotPattern2 from "../assets/dot_pattern_2.png";
+import dotPattern1 from "../assets/dot_pattern_4.png";
+import dotPattern2 from "../assets/dot_pattern_7.png";
+import dotPattern3 from "../assets/dot_pattern_1.png";
 import bottomPattern from "../assets/partners/border_style_bottom.png";
 import topPattern from "../assets/partners/border_style_top.png";
 import partnerImg1 from "../assets/partners/img-1.png";
 import partnerImg2 from "../assets/partners/img-2.png";
+import partnerImg3 from "../assets/partners/img-4.png";
 
 function Partners() {
-  return (
-    <Container className="supportingPartners" id="support">
-      <img alt="" src={dotPattern2} className="dotPatternPartnerMid" />
-      <img alt="" src={bottomPattern} className="dotPatternPartnerBot" />
-      <img alt="" src={topPattern} className="dotPatternPartnerTop" />
-      <img alt="" src={dotPattern1} className="dotPatternPartnerBack" />
-      <img alt="" src={dotPattern1} className="dotPatternPartnerBack2" />
-      <h2 className="sectionTitle">SUPPORTING PARTNERS</h2>
-      <div className="partnersLogos">
-        <img alt="" src={partnerImg1} className="partnersImg" />
-        <img alt="" src={partnerImg2} className="partnersImg" />
-      </div>
-      {/*<div>
-        <Button className="buttonGeneral buttonGeneralSignup">MORE</Button>
-  </div> */}
-    </Container>
+    return (
+        <Container className="supportingPartners" id="support">
+            <img alt="" src={dotPattern2} className="dotPatternPartnerMid"/>
+            <img alt="" src={bottomPattern} className="dotPatternPartnerBot"/>
+            <img alt="" src={topPattern} className="dotPatternPartnerTop"/>
+            <img alt="" src={dotPattern1} className="dotPatternPartnerBack"/>
+            <img alt="" src={dotPattern3} className="dotPatternPartnerBack2"/>
+            <img alt="" src={dotPattern3} className="dotPatternPartnerBack3"/>
+            <img alt="" src={dotPattern1} className="dotPatternPartnerBack4"/>
+            <h2 className="sectionTitle">SUPPORTING PARTNERS</h2>
+            <div className="partnersLogos">
+                <img alt="" src={partnerImg1} className="partnersImg"/>
+                <img alt="" src={partnerImg2} className="partnersImg"/>
+                <img alt="" src={partnerImg3} className="partnersImg"/>
+            </div>
+            <div>
+                <Button className="buttonGeneral buttonGeneralSignup">MORE</Button>
+            </div>
+        </Container>
   );
 }
 
